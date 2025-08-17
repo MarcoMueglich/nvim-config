694237eebb5b0771c0eb8d7b92aa0bb7c6b743e9
MarcoMueglich <marco23498@gmail.com>
2025-08-17 18:03:11 +0200

    added modular plugins

--- init.lua
+++ init.lua
@@ -1,5 +1,5 @@
 require("keymaps")
 require("options")
-require("plugins.lazy")
-require("plugins.keymaps")
-require("plugins.options")
+require("config.lazy")
+require("config.keymaps")
+require("config.options")
