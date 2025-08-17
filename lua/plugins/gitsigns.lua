f1d1c2f1a326c114637b2342b35f96f19d570d8a
MarcoMueglich <marco23498@gmail.com>
2025-08-17 19:21:43 +0200

    added git blame

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -8,7 +8,14 @@ return { -- Adds git related signs to the gutter, as well as utilities for manag
         topdelete    = { text = '‾' },
         changedelete = { text = '~' },
         untracked    = { text = '┆' },
-    },
-    },
+    	},
+      current_line_blame = true, -- Show blame on the current line
+      current_line_blame_opts = {
+	virt_text_pos = 'eol', -- Position of the blame text
+        virt_text = true,
+	delay = 1000, -- Delay before showing blame
+   	 },
+      current_line_blame_formatter = '  <author>, <author_time:%Y-%m-%d> - <summary>', -- Format for the blame text
+	}
   }
 
