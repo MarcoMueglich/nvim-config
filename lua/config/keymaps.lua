4f07806008595690af8d2df7987f59fe7d479191
MarcoMueglich <marco23498@gmail.com>
2025-08-17 22:12:11 +0200

    added more plugins

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,3 +6,17 @@ vim.keymap.set("n", "<leader>e", ":NvimTreeFindFileToggle<cr>", { desc = "Toggle
 
 -- comment
 vim.keymap.set("n", "<C-#>", ":CommentToggle<cr>", { desc = "Toggle comment" })
+
+-- move lines
+local opts = { noremap = true, silent = true }
+-- Normal-mode commands
+vim.keymap.set("n", "<A-Down>", ":MoveLine 1<CR>", opts)
+vim.keymap.set("n", "<A-Up>", ":MoveLine -1<CR>", opts)
+vim.keymap.set("n", "<A-S-Left>", ":MoveWord -1<CR>", opts)
+vim.keymap.set("n", "<A-S-Right>", ":MoveWord 1<CR>", opts)
+
+-- Visual-mode commands
+vim.keymap.set("x", "<A-Down>", ":MoveBlock 1<CR>", opts)
+vim.keymap.set("x", "<A-Up>", ":MoveBlock -1<CR>", opts)
+vim.keymap.set("v", "<A-Left>", ":MoveHBlock -1<CR>", opts)
+vim.keymap.set("v", "<A-Right>", ":MoveHBlock 1<CR>", opts)
