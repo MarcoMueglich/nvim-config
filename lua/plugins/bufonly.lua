0fcd7cb41ce1aecc52c5e1005fa03db2860d9d7e
Marco Müglich <m.mueglich@bm-systems.com>
2025-08-18 13:35:00 +0200

    changed git format

---
+++ lua/plugins/bufonly.lua
@@ -0,0 +1,1 @@
+return { "numtostr/BufOnly.nvim" }
--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,21 +1,20 @@
 return { -- Adds git related signs to the gutter, as well as utilities for managing changes
-    'lewis6991/gitsigns.nvim',
-    opts = {
-      signs = {    
-        add          = { text = '┃' },
-        change       = { text = '┃' },
-        delete       = { text = '_' },
-        topdelete    = { text = '‾' },
-        changedelete = { text = '~' },
-        untracked    = { text = '┆' },
-    	},
-      current_line_blame = true, -- Show blame on the current line
-      current_line_blame_opts = {
-	virt_text_pos = 'eol', -- Position of the blame text
-        virt_text = true,
-	delay = 1000, -- Delay before showing blame
-   	 },
-      current_line_blame_formatter = '  <author>, <author_time:%Y-%m-%d> - <summary>', -- Format for the blame text
-	}
-  }
-
+	"lewis6991/gitsigns.nvim",
+	opts = {
+		signs = {
+			add = { text = "┃" },
+			change = { text = "┃" },
+			delete = { text = "_" },
+			topdelete = { text = "‾" },
+			changedelete = { text = "~" },
+			untracked = { text = "┆" },
+		},
+		current_line_blame = true, -- Show blame on the current line
+		current_line_blame_opts = {
+			virt_text_pos = "eol", -- Position of the blame text
+			virt_text = true,
+			delay = 1000, -- Delay before showing blame
+		},
+		current_line_blame_formatter = "  <author>, <author_time:%d.%m.%Y> - <summary>", -- Format for the blame text
+	},
+}
